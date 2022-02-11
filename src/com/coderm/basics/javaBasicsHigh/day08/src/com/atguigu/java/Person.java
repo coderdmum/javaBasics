40e938f8ff8a517acf279b6ba7bd9434fb3387b0
coderm <coderm@163.com>
2022-02-11 14:35:38 +0800

    2022-01-11-1543

---
+++ src/com/coderm/basics/javaBasicsHigh/day08/src/com/atguigu/java/Person.java
@@ -0,0 +1,8 @@
+package com.coderm.basics.javaBasicsHigh.day08.src.com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2019 上午 11:24
+ */
+public class Person {
+}
---
+++ src/com/coderm/basics/javaBasicsHigh/day08/src/com/atguigu/java2/Student.java
@@ -0,0 +1,8 @@
+package com.coderm.basics.javaBasicsHigh.day08.src.com.atguigu.java2;
+
+/**
+ * @author shkstart
+ * @create 2019 下午 2:42
+ */
+public class Student extends Person {
+}
